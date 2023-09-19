fc10216fa76f4ed79b01ddfd79021868f7123491
subhadeepdhar17 <oliverdepp17@gmail.com>
2023-09-19 21:09:46 +0530

    Minor changes

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (lat, long, callback) => {
             callback('Location not found', undefined)
         } else {
             callback(undefined, response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature
-            + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees.')
+            + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees. Humidity is ' + response.body.current.humidity + '%.')
         }
     })
 }
